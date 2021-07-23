e2617133740c30e54006637e1ca8d35c31e9d745
Yukio Nozawa <personal@nyanchangames.com>
2021-07-24 00:23:12 +0900

    Start adding logic tests

---
+++ test/chatHandler_test.ts
@@ -0,0 +1,12 @@
+import { expect } from "chai";
+import { ChatHandler } from "../src/logic/chatHandler";
+import * as dfgmsg from "../msg-src/dfgmsg";
+
+describe("ChatHandler", () => {
+  it("can generate chat message from request and player name", () => {
+    const req = dfgmsg.encodeChatRequest("test");
+    const msg = new ChatHandler().generateChatMessage(req, "cat");
+    expect(msg.playerName).to.eql("cat");
+    expect(msg.message).to.eql("test");
+  });
+});
---
+++ test/decodeValidator_test.ts
@@ -0,0 +1,23 @@
+import { expect } from "chai";
+import { isDecodeSuccess } from "../src/logic/decodeValidator";
+import * as dfgmsg from "../msg-src/dfgmsg";
+
+describe("DecodeValidator", () => {
+  it("returns true when payload is successfully decoded", () => {
+    const encoded = dfgmsg.encodeChatRequest("test");
+    const decoded = dfgmsg.decodePayload<dfgmsg.ChatRequest>(
+      encoded,
+      dfgmsg.ChatRequestDecoder
+    );
+    expect(isDecodeSuccess<dfgmsg.ChatRequest>(decoded)).to.be.true;
+  });
+
+  it("returns false when payload is invalid", () => {
+    const encoded = { invalid: true };
+    const decoded = dfgmsg.decodePayload<dfgmsg.ChatRequest>(
+      encoded,
+      dfgmsg.ChatRequestDecoder
+    );
+    expect(isDecodeSuccess<dfgmsg.ChatRequest>(decoded)).to.be.false;
+  });
+});
