e16546682a0da6ba9f09b2d98bfca1376e0ab1c8
Yukio Nozawa <personal@nyanchangames.com>
2021-07-31 18:41:16 +0900

    Add cardEnumerator

---
+++ src/logic/cardEnumerator.ts
@@ -0,0 +1,19 @@
+import * as dfg from "dfg-simulator";
+import * as dfgmsg from "../../msg-src/dfgmsg";
+
+export class CardEnumerator {
+  public enumerate(
+    hand: dfg.Hand,
+    activePlayerControl: dfg.ActivePlayerControl | null
+  ): dfgmsg.CardListMessage {
+    return this.inactive(hand);
+  }
+
+  private inactive(hand: dfg.Hand): dfgmsg.CardListMessage {
+    return dfgmsg.encodeCardListMessage(
+      hand.cards.map((v) => {
+        return dfgmsg.encodeCardMessage(v.mark, v.cardNumber, false, false);
+      })
+    );
+  }
+}
---
+++ test/cardEnumerator_test.ts
@@ -0,0 +1,24 @@
+import { expect } from "chai";
+import { CardEnumerator } from "../src/logic/cardEnumerator";
+import * as dfg from "dfg-simulator";
+
+describe("CardEnumerator", () => {
+  it("can enumerate cards when player is inactive", () => {
+    const ce = new CardEnumerator();
+    const c1 = dfg.createCard(dfg.CardMark.SPADES, 4);
+    const c2 = dfg.createCard(dfg.CardMark.SPADES, 5);
+    const h = dfg.createHand();
+    h.give(c1, c2);
+    const msg = ce.enumerate(h, null);
+    expect(msg.cardList).not.to.be.null;
+    const cl = msg.cardList;
+    expect(cl[0].markEnum).to.eql(dfg.CardMark.SPADES)
+    expect(cl[0].cardNumber).to.eql(4);
+    expect(cl[0].isChecked).to.be.false;
+    expect(cl[0].isCheckable).to.be.false;
+	expect(cl[1].markEnum).to.eql(dfg.CardMark.SPADES)
+    expect(cl[1].cardNumber).to.eql(5);
+    expect(cl[1].isChecked).to.be.false;
+    expect(cl[1].isCheckable).to.be.false;
+  });
+});
