034165aefc330a1bddaf4f80a44513ae605eca24
Yukio Nozawa <personal@nyanchangames.com>
2021-08-06 00:21:30 +0900

    Add updateCardsForEveryone test

--- test/dfgHandler_test.ts
+++ test/dfgHandler_test.ts
@@ -11,6 +11,14 @@ function createDFGHandler(): DFGHandler {
   return h;
 }
 
+function createCards(): dfg.Card[] {
+  return [
+    dfg.createCard(dfg.CardMark.SPADES, 4),
+    dfg.createCard(dfg.CardMark.SPADES, 5),
+    dfg.createCard(dfg.CardMark.SPADES, 6),
+  ];
+}
+
 function createGame() {
   const g = <dfg.Game>(<unknown>{
     enumeratePlayerIdentifiers: sinon.fake(() => {
@@ -41,5 +49,39 @@ describe("DFGHandler", () => {
     h["createGame"] = cg;
     h.startGame(["a", "b", "c"]);
     expect(cg.called).to.be.true;
+  });
+
+  it("can update hand info for everyone", () => {
+    const p1 = dfg.createPlayer("a");
+    p1.hand.give(...createCards());
+    const p2 = dfg.createPlayer("b");
+    p2.hand.give(...createCards());
+    const p3 = dfg.createPlayer("c");
+    p3.hand.give(...createCards());
+    const h = createDFGHandler();
+    const g = <dfg.Game>(<unknown>{
+      enumeratePlayerIdentifiers: sinon.fake(() => {
+        return ["a", "b", "c"];
+      }),
+      findPlayerByIdentifier: sinon.fake((identifier: string) => {
+        switch (identifier) {
+          case "a":
+            return p1;
+          case "b":
+            return p2;
+          case "c":
+            return p3;
+        }
+        throw new Error("error");
+      }),
+    });
+    h.game = g;
+    const roomProxyMock = sinon.mock(h.roomProxy);
+    roomProxyMock.expects("send").thrice();
+    const cardEnumeratorMock = sinon.mock(h.cardEnumerator);
+    cardEnumeratorMock.expects("enumerate").thrice();
+    h.updateCardsForEveryone();
+    roomProxyMock.verify();
+    cardEnumeratorMock.verify();
   });
 });
