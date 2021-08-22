245a4f46f0e031525211722dead2b7285c6a4c03
Yukio Nozawa <personal@nyanchangames.com>
2021-08-22 14:50:16 +0900

    Fix lint

--- src/logic/authAdapters.ts
+++ src/logic/authAdapters.ts
@@ -3,11 +3,14 @@
 // セッションを使う場合は、 SessionAdapter を使う用にする。
 
 export interface AuthAdapter {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   authorize: (clientOptions: any) => boolean;
 }
 
 export class nameOnlyAdapter implements AuthAdapter {
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
   public authorize(clientOptions: any): boolean {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     if (!clientOptions.playerName) {
       return false;
     }
