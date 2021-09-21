f3a410e5c7400c029a3dfaffd274c277da473b85
Yukio Nozawa <personal@nyanchangames.com>
2021-09-21 23:23:21 +0900

    Rename playerNames to playerList

--- src/rooms/schema/result.ts
+++ src/rooms/schema/result.ts
@@ -1,9 +1,9 @@
 import { Schema, ArraySchema, type } from "@colyseus/schema";
 
 export class Result extends Schema {
-  @type(["string"]) daifugoPlayerNames = new ArraySchema<string>();
-  @type(["string"]) fugoPlayerNames = new ArraySchema<string>();
-  @type(["string"]) heiminPlayerNames = new ArraySchema<string>();
-  @type(["string"]) hinminPlayerNames = new ArraySchema<string>();
-  @type(["string"]) daihinminPlayerNames = new ArraySchema<string>();
+  @type(["string"]) daifugoPlayerList = new ArraySchema<string>();
+  @type(["string"]) fugoPlayerList = new ArraySchema<string>();
+  @type(["string"]) heiminPlayerList = new ArraySchema<string>();
+  @type(["string"]) hinminPlayerList = new ArraySchema<string>();
+  @type(["string"]) daihinminPlayerList = new ArraySchema<string>();
 }
